90a7a22c423f306e287eb2fdeedc585cbd17f6f9
litengat <litengut@icloud.com>
2024-08-18 00:36:39 +0000

    chore: Remove unused demo page component

--- src/app/demo/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import React from "react";
-import { Button } from "@/components/ui/button";
-import { testpermissons } from "@/server/user";
-
-export default function BackgroundBeamsDemo() {
-  return <></>;
-}
